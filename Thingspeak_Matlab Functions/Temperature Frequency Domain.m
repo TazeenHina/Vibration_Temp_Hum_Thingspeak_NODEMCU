fa814e7dc0cfcded7b1d1b87241ab2ed26e82959
hrasul <77195196+hrasul@users.noreply.github.com>
2022-07-01 22:26:17 +0500

    Update Temperature Frequency Domain.m

--- Thingspeak_Matlab Functions/Temperature Frequency Domain.m
+++ Thingspeak_Matlab Functions/Temperature Frequency Domain.m
@@ -1,4 +1,4 @@
-[data,timestamps,channelInfo]= thingSpeakRead(1764189,'Fields',3,DateRange=[datetime(2022,6,14,23,59,01),datetime(2022,6,15,23,59,52)],ReadKey='4EFU2H1TU2IJS1N0');
+[data,timestamps,channelInfo]= thingSpeakRead(,'Fields',3,DateRange=[datetime(2022,6,14,23,59,01),datetime(2022,6,15,23,59,52)],ReadKey='');
 
 a=fft(data);
 
